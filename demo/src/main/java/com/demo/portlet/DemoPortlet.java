29b471eddb715e0c9d2d478e983644fd91178336
gciacciarelli-hub <60382986+gciacciarelli-hub@users.noreply.github.com>
2020-01-29 21:39:34 +0100

    first

---
+++ demo/src/main/java/com/demo/portlet/DemoPortlet.java
@@ -0,0 +1,30 @@
+package com.demo.portlet;
+
+import com.demo.constants.DemoPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Giuseppe
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=Demo",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + DemoPortletKeys.DEMO,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user", "com.demo.name=My First Portlet"
+	},
+	service = Portlet.class
+)
+public class DemoPortlet extends MVCPortlet {
+}
